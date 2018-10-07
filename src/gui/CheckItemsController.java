5e6be051bb8df2412b2e096523d86948dea71837
Lang <langdk@ad.msoe.edu>
2018-10-07 14:31:23 -0500

    Added more tabs to check items page, and updated the GUI

--- src/gui/CheckItemsController.java
+++ src/gui/CheckItemsController.java
@@ -1,11 +1,16 @@
 package gui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +19,9 @@ public class CheckItemsController implements Initializable{
 
     @FXML
     TableColumn studentID;
+
+    @FXML
+    MenuItem quit;
 
 
 
@@ -27,5 +35,8 @@ public class CheckItemsController implements Initializable{
         VBox headerGraphic = new VBox();
         headerGraphic.setAlignment(Pos.CENTER);
         studentID.setGraphic(headerGraphic);
+    }
+    public void close(){
+        System.out.println("HI");
     }
 }
