29ddbed35eaaed7695514e3a3f0f478aa4a6d7ad
Matthew Karcz <karczma@msoe.edu>
2018-10-10 09:05:55 -0500

    Added Part.java from the checkin branch so that we may use it for displaying parts

---
+++ src/gui/ControllerInventory.java
@@ -0,0 +1,36 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerInventory implements Initializable {
+
+    @FXML
+    private Button part;
+
+    @FXML
+    private Button back;
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+
+}
