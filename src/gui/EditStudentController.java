4f5bea6ce2ec5d2d182aebf414820725b4c2c512
terrybc <terrybc@msoe.edu>
2018-10-15 08:39:52 -0500

    edited imports to allow for window to pop up and made a method to check if its a valid date

--- src/gui/EditStudentController.java
+++ src/gui/EditStudentController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
-import java.awt.*;
+import javafx.scene.control.TextField;
 
 public class EditStudentController {
 
@@ -12,4 +15,14 @@ public class EditStudentController {
 
     @FXML
     ListView checkedOut, savedItems, overdueItems;
+
+    @FXML
+    private void checkDate(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)){
+            if(!dateOfRental.getText().matches("[0-1][0-9]\\/[0-3][0-9]\\/[0-9]{4}")){
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Error, invalid date was entered");
+                alert.showAndWait();
+            }
+        }
+    }
 }
