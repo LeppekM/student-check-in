d7a52a456db767784fe13d903ad9f8a6957f286e
Bailey Terry <terrybc@msoe.edu>
2018-10-19 22:05:17 +0000

    ExtendedPart.java edited online with Bitbucket

--- src/gui/ExtendedPart.java
+++ src/gui/ExtendedPart.java
@@ -1,34 +1,49 @@
 package gui;
 
 public class ExtendedPart extends Part {
-
+ 
     private final SimpleStringProperty professor;
-    
++<<<<<<< destination:d3db1937de8c018f656443f0bbe37b60437ccb3c
+ 
     public ExtendedPart(String partName, long serialNumber, String manufacturer, int quantity, double price, String vendor, String location, String barcode, boolean fault, long studentId, String prof){
         super(partName, serialNumber, manufacturer, quantity, price, vendor, location, barcode, fault, studentId);
         this.professor = new SimpleStringProperty(prof);
     }
- 
+
     public String getName() {
         return this.professor.get();
     }
  
     public void setName(String prof) {
         this.professor.set(prof);
-    
-    public static String getCourseID() {
-        return courseID;
-    }
-
-    public static void setCourseID(String courseID) {
-        ExtendedPart.courseID = courseID;
-    }
-
-    public static Date getDueDate() {
-        return dueDate;
-    }
-
-    public static void setDueDate(String dueDate) {
-        this.dueDate = dueDate;
++=======
++    
++    public ExtendedPart(String partName, long serialNumber, String manufacturer, int quantity, double price, String vendor, String location, String barcode, boolean fault, long studentId, String prof){
++        super(partName, serialNumber, manufacturer, quantity, price, vendor, location, barcode, fault, studentId);
++        this.professor = new SimpleStringProperty(prof);
++    }
++ 
++    public String getName() {
++        return this.professor.get();
++    }
++ 
++    public void setName(String prof) {
++        this.professor.set(prof);
++    
++    public static String getCourseID() {
++        return courseID;
++    }
++
++    public static void setCourseID(String courseID) {
++        ExtendedPart.courseID = courseID;
++    }
++
++    public static Date getDueDate() {
++        return dueDate;
++    }
++
++    public static void setDueDate(String dueDate) {
++        this.dueDate = dueDate;
++>>>>>>> source:16bc91f8230788fcbda218ed522ef2894f76d637
     }
 }
