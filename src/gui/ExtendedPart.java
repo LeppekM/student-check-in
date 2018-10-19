9e81758ff37ddeb2cdc5b42814941bf00f9c3342
Bailey Terry <terrybc@msoe.edu>
2018-10-19 22:00:01 +0000

    ExtendedPart.java edited online with Bitbucket

--- src/gui/ExtendedPart.java
+++ src/gui/ExtendedPart.java
@@ -3,24 +3,20 @@ package gui;
 public class ExtendedPart extends Part {
 
     private final SimpleStringProperty professor;
-    private Date dueDate;
-    private String courseID;
-
-    public ExtendedPart(String partName, long serialNumber, String manufacturer, int quantity, double price, String vendor, String location, String barcode, boolean fault, long studentId, String prof, String courseID, Date dueDate) {
-        super(name, location, price, serialNumber, barcode, vendor, manufacturer, fault, studentID);
+    
+    public ExtendedPart(String partName, long serialNumber, String manufacturer, int quantity, double price, String vendor, String location, String barcode, boolean fault, long studentId, String prof){
+        super(partName, serialNumber, manufacturer, quantity, price, vendor, location, barcode, fault, studentId);
         this.professor = new SimpleStringProperty(prof);
-        this.dueDate = dueDate;
-        this.courseID = courseID;
     }
-
-    public String getProf() {
+ 
+    public String getName() {
         return this.professor.get();
     }
-
-    public void setProf(String prof) {
+ 
+    public void setName(String prof) {
         this.professor.set(prof);
     }
-
+    
     public static String getCourseID() {
         return courseID;
     }
