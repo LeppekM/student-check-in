d0c257538b365f61c253d8323c79938e40996a69
Matthew Karcz <karczma@msoe.edu>
2018-10-24 08:31:17 -0500

    Deleted source from spike to make room for future sprints

---
+++ src/StudentCheckin.java
@@ -0,0 +1,16 @@
+public class StudentCheckin extends Application  {
+    public StudentCheckin(){
+
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
