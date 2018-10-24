05dd47aa4c8531639bf47a6e191fe0ddfd018c75
Matthew Karcz <karczma@msoe.edu>
2018-10-24 08:41:29 -0500

    Set src and test folders and updated StudentCheckin.java to remove error

--- src/StudentCheckin.java
+++ src/StudentCheckin.java
@@ -1,6 +1,3 @@
-package gui;
-
-import database.Database;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,15 +16,6 @@ public class StudentCheckin extends Application  {
         primaryStage.setTitle("Barcode Scanner");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-//        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
-//
-//        Scene scene = new Scene(root, 800, 600);
-//
-//        primaryStage.setTitle("Barcode Scanner");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-
     }
 
 
