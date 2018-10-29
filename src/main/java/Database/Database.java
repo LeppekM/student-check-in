c52bb52dee1dfd67369ef6b7e6d129a9f36758fc
terrybc <terrybc@msoe.edu>
2018-10-29 16:48:04 -0500

    starting to connect computers

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,7 +1,11 @@
 package Database;
 
+import java.sql.Connection;
+
 public class Database {
 
     static String host = "jdbc:mysql://192.168.56.1:3306/eqod-log";
-    static Sting
+    static final String dbdriver = "com.mysql.jdbc.Driver";
+    static final String dbname = "parts";
+    static Connection connection;
 }
