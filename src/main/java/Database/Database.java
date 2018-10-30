e8080edafef7bcf1dd5e4f534349522beb7f0b12
terrybc <terrybc@msoe.edu>
2018-10-29 19:25:21 -0500

    found out the overdue page doesnt display while writing code for database

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,11 +1,57 @@
 package Database;
 
+import javax.swing.*;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class Database {
 
-    static String host = "jdbc:mysql://192.168.56.1:3306/eqod-log";
+    static String host = "jdbc:mysql://192.168.56.1:3306";
     static final String dbdriver = "com.mysql.jdbc.Driver";
     static final String dbname = "parts";
     static Connection connection;
+
+    public void test() {
+        // scanner.useDelimiter("\n");
+
+        String login = JOptionPane.showInputDialog("Enter login name: ");
+
+        // Note: password will be echoed to console;
+        String password = JOptionPane.showInputDialog("Enter password: ");
+
+        // String password = PasswordField.readPassword("Enter password: ");
+
+        JOptionPane.showMessageDialog(null,"Connecting as user '" + login + "' . . .");
+
+        // Load the JDBC driver.
+        // Library (.jar file) must be added to project build path.
+        try {
+            Class.forName(dbdriver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        connection = null;
+        try {
+            connection = DriverManager.getConnection((host + "/" + dbname),
+                    login, password);
+            connection.setClientInfo("autoReconnect", "true");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        try {
+            String test = "update vendor v set v.vendorID = 1, v.vendor = 'bob'";
+            Statement preparedStatement = connection.createStatement();
+            preparedStatement.executeUpdate(test.toString());
+            preparedStatement.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/InventoryController/ControllerOverdueTab.java
+++ src/main/java/InventoryController/ControllerOverdueTab.java
@@ -1,12 +1,38 @@
 package InventoryController;
 
+import Database.Database;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 
+import javax.swing.*;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerOverdueTab  extends ControllerInventoryPage implements Initializable {
+
+    @FXML
+    TableColumn studentID, partID, serial, date, fee;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+//        Database d = new Database();
+//        d.test();
+//        JOptionPane.showMessageDialog(null, "Done");
+        try {
+            File file = new File("/src/testOverdue.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null){
+                studentID.getColumns().add(1, line.substring(0, line.indexOf(',')));
+            }
+            bufferedReader.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
