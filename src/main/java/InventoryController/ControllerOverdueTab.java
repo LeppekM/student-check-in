cf00568165ace98ce57351c2dd71a2172747e402
terrybc <terrybc@msoe.edu>
2018-10-29 20:27:02 -0500

    tried to connect computer to my sql

--- src/main/java/InventoryController/ControllerOverdueTab.java
+++ src/main/java/InventoryController/ControllerOverdueTab.java
@@ -4,6 +4,7 @@ import Database.Database;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import javax.swing.*;
 import java.io.*;
@@ -17,22 +18,22 @@ public class ControllerOverdueTab  extends ControllerInventoryPage implements In
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//        Database d = new Database();
-//        d.test();
-//        JOptionPane.showMessageDialog(null, "Done");
-        try {
-            File file = new File("java/testOverdue.txt");
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = "";
-            while ((line = bufferedReader.readLine()) != null){
-                studentID.getColumns().add(1, line.substring(0, line.indexOf(',')));
-            }
-            bufferedReader.close();
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        Database d = new Database();
+        d.test();
+        JOptionPane.showMessageDialog(null, "Done");
+//        try {
+//            File file = new File("testOverdue.txt");
+//            FileReader fileReader = new FileReader(file);
+//            BufferedReader bufferedReader = new BufferedReader(fileReader);
+//            String line = "";
+//            while ((line = bufferedReader.readLine()) != null){
+//
+//            }
+//            bufferedReader.close();
+//        }catch (FileNotFoundException e){
+//            e.printStackTrace();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
     }
 }
