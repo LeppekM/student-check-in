c37d6b4ed52f0405c80e27ea4c69a727c3617e3e
Lang <langdk@ad.msoe.edu>
2018-11-03 19:43:19 -0500

    Cleaned up method, and added confirmation dialogs/closing of window when action is successful

--- src/main/java/Database/AddPart.java
+++ src/main/java/Database/AddPart.java
@@ -10,26 +10,33 @@ public class AddPart {
     private String addQuery = "INSERT INTO parts(partID, partName, serialnumber, manufacturer, price, vendorID," +
             " location, barcode, totalQuantity,  availableQuantity, createdAt, createdBy)"+
             "VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+    private String getpartIDQuery = "SELECT partID\n" +
+            "FROM parts\n" +
+            "ORDER BY partID DESC\n" +
+            "LIMIT 1";
 
-
+    /**
+     * This method adds an item to the database
+     * @param part The part to be added
+     */
     public void addItem(Part part){
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             PreparedStatement preparedStatement = connection.prepareStatement(addQuery);
             insertQuery(part, preparedStatement).execute();
-//            ResultSet resultSet = statement.executeQuery(SELECTQUERY);
-//            while(resultSet.next()){
-//                setVariables(resultSet);
-//                CheckedOutItems checkedOutItems = new CheckedOutItems(studentName, partName, checkoutQuantity, checkedOutAt, dueDate);
-//                data.add(checkedOutItems);
-//            }
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect to the database", e);
         }
     }
 
+    /**
+     * This method sets the information from a part to the item being added to the database
+     * @param part The part being added to the database
+     * @param preparedStatement The statement that has items being set to it
+     * @return
+     */
     private PreparedStatement insertQuery(Part part, PreparedStatement preparedStatement){
         try {
-            preparedStatement.setInt(1, 5);
+            preparedStatement.setInt(1, getPartID());
             preparedStatement.setString(2, part.getPartName());
             preparedStatement.setString(3, part.getSerialNumber());
             preparedStatement.setString(4, part.getManufacturer());
@@ -44,12 +51,36 @@ public class AddPart {
             //Hardcoded created by because we don't have workers setup yet
             preparedStatement.setString(12, "John");
         }catch (SQLException e){
-            e.printStackTrace();
+            throw new IllegalStateException("Cannot connect to the database", e);
         }
         return preparedStatement;
     }
 
+    /**
+     * This method gets the current date
+     * @return Current date
+     */
     private String getCurrentDate(){
         return LocalDateTime.now().toString();
+    }
+
+    /**
+     * This method queries the database and finds the max ID. It then increments this id to return
+     * the next partID when adding a part
+     * @return The new part ID to be added to the database for the corresponding part
+     */
+    private int getPartID(){
+        int partID = 0;
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(getpartIDQuery);
+            while(rs.next()){
+                partID = rs.getInt("partID");
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect to the database", e);
+        }
+        //Gets max id from table, then increments to create new partID
+        return partID + 1;
     }
 }
--- src/main/java/InventoryController/ControllerAddPart.java
+++ src/main/java/InventoryController/ControllerAddPart.java
@@ -4,13 +4,16 @@ import Database.AddPart;
 import Database.Part;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerAddPart extends ControllerInventoryPage implements Initializable {
-
+    @FXML
+    private VBox sceneAddPart;
 
     @FXML
     private TextField nameField;
@@ -40,11 +43,18 @@ public class ControllerAddPart extends ControllerInventoryPage implements Initia
 
     }
 
+    /**
+     * Adds the part to database
+     */
     public void submitItem(){
         addPart.addItem(setPartFields());
-
+        partAddedSuccess();
     }
 
+    /**
+     * Helper method that sets all the part information that will be added to the database
+     * @return The part to be added to database
+     */
     private Part setPartFields(){
         String partname = nameField.getText();
         String serialNumber = serialField.getText();
@@ -54,9 +64,31 @@ public class ControllerAddPart extends ControllerInventoryPage implements Initia
         String location = locationField.getText();
         String barcode = serialField.getText();
         int quantity = Integer.parseInt(quantityField.getText());
-
-
         return new Part(partname, serialNumber, manufacturer, price, vendor, location, barcode, quantity);
+    }
+
+    /**
+     * Creates an alert informing user that part was added successfully
+     */
+    private void partAddedSuccess(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Confirmation");
+        alert.setContentText("Part added successfully");
+        alert.showAndWait();
+    }
+
+    /**
+     * Returns to main inventory page
+     */
+    public void goBack(){
+        close();
+    }
+
+    /**
+     * Helper method to close platform
+     */
+    private void close(){
+        sceneAddPart.getScene().getWindow().hide();
     }
 
 
