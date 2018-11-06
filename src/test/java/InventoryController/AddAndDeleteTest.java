8e95496d02ffd9d01fae1c32aa9c19805db53a0b
terrybc <terrybc@msoe.edu>
2018-11-06 15:26:30 -0600

    changed test to not use my credentials

--- src/test/java/InventoryController/AddAndDeleteTest.java
+++ src/test/java/InventoryController/AddAndDeleteTest.java
@@ -14,14 +14,14 @@ public class AddAndDeleteTest {
 
     @Before
     public void before(){
-        database = new Database("terrybc", "Sharks$199714");
+        database = new Database("root", "Rootpass123");
     }
 
     @Test
     public void addParts(){
         AddPart addPart = new AddPart();
         Part part = new Part("testPart", "serial", "manufacturer",0.00, "2", "location", "barcode", false, 100, false);
-        addPart.addItem(part,"terrybc", "Sharks$199714");
+        addPart.addItem(part,"root", "Rootpass123");
         Part test = database.selectPart(100);
         assertEquals(part.getPartName(), test.getPartName());
         assertEquals(part.getSerialNumber(), test.getSerialNumber());
