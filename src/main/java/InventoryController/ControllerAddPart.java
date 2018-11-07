00abf1d83802cc0f65a562a5667608d47ac68f13
Lang <langdk@ad.msoe.edu>
2018-11-07 12:34:56 -0600

    Updated add part method so that invalid input will not crash the program

--- src/main/java/InventoryController/ControllerAddPart.java
+++ src/main/java/InventoryController/ControllerAddPart.java
@@ -48,25 +48,156 @@ public class ControllerAddPart extends ControllerInventoryPage implements Initia
      * Adds the part to database
      */
     public void submitItem(){
+
+
+        if(validateFieldsNotEmpty() && validateQuantityField() && validatePriceField()){
+        setPartFields();
         addPart.addItem(setPartFields(), DatabaseLogin.username, DatabaseLogin.password);
         partAddedSuccess();
         close();
+        }
+        else {
+            errorHandler();
+        }
     }
+
+
 
     /**
      * Helper method that sets all the part information that will be added to the database
      * @return The part to be added to database
      */
     private Part setPartFields(){
+        String failedCheck = "-1";
+
         String partname = nameField.getText();
         String serialNumber = serialField.getText();
         String manufacturer = manufacturerField.getText();
-        double price = Double.parseDouble(priceField.getText());
+        String price = priceField.getText();
         String vendor = vendorField.getText();
         String location = locationField.getText();
         String barcode = serialField.getText();
-        int quantity = Integer.parseInt(quantityField.getText());
-        return new Part(partname, serialNumber, manufacturer, price, vendor, location, barcode, quantity);
+        String quantity = quantityField.getText();
+        //If the price or quantity isn't filled out, the invalid value -1 is passed instead.
+        if(price.isEmpty()){
+            price = failedCheck;
+        }
+        if (quantity.isEmpty()){
+            quantity = failedCheck;
+        }
+
+        return new Part(partname, serialNumber, manufacturer, priceCheck(price), vendor, location, barcode, quantityCheck(quantity));
+    }
+
+
+    /**
+     * Checks if the input entered is a double
+     * @param price The double to be returned. Returns -1 if not a double, and the value otherwise
+     * @return
+     */
+    private double priceCheck(String price){
+        double positivePriceCheck;
+        double failedvalue = -1;
+        try {
+            positivePriceCheck = Double.valueOf(price); //If price is a valid double
+        }
+        catch (Exception e){
+            return failedvalue;
+        }
+        if(positivePriceCheck > 0){ //If price is greater than 0
+            return positivePriceCheck;
+        }
+        return failedvalue;
+    }
+
+    /**
+     * Checks if the input entered is an integer
+     * @param quantity The integer to be returned. Returns -1 if not an int, and the value otherwise
+     * @return
+     */
+    private int quantityCheck(String quantity){
+        int positiveCheck;
+        int failedValue = -1;
+        if(quantity.chars().allMatch(Character::isDigit)){ //If quantity is a valid int
+             positiveCheck = Integer.parseInt(quantity);
+            if (positiveCheck >0){ //If quantity is greater than 0
+                return positiveCheck;
+            }
+        }
+        else {
+            return failedValue;
+        }
+        return failedValue;
+    }
+
+    /**
+     * Determines if the price textfield input is valid or not
+     * @return True if the price is valid
+     */
+    private boolean validatePriceField(){
+        if(priceCheck(priceField.getText())==-1){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Determines if the quantity textfield input is valid or not
+     * @return True if the quantity is valid
+     */
+    private boolean validateQuantityField(){
+        if(quantityCheck(quantityField.getText())==-1){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This checks to see if the textfields are empty
+     * @return False if any field is empty
+     */
+    private boolean validateFieldsNotEmpty(){
+        if(nameField.getText().isEmpty() | serialField.getText().isEmpty() | manufacturerField.getText().isEmpty() |
+        priceField.getText().isEmpty() | vendorField.getText().isEmpty() | locationField.getText().isEmpty()|
+        serialField.getText().isEmpty()| quantityField.getText().isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Helper method to select correct error dialog based on what went wrong
+     */
+    private void errorHandler(){
+        if(!validateFieldsNotEmpty()){
+            fieldErrorAlert();
+        }
+        else if (!validateQuantityField()|| !validatePriceField()){
+            invalidNumberAlert();
+        }
+    }
+
+
+    /**
+     * Creates an alert informing user to fill out all fields
+     */
+    private void fieldErrorAlert(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setContentText("Please fill out all fields before submitting info");
+
+        alert.showAndWait();
+    }
+
+    /**
+     * Creates alert that informs user invalid input was entered into price or quantity field
+     */
+    private void invalidNumberAlert(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setContentText("Please make sure you are entering numbers into price and quantity fields, and that they are not negative");
+
+        alert.showAndWait();
     }
 
     /**
