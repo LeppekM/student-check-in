635d2de6937ff8e026dbde03eeed80c475d9e122
Lang <langdk@ad.msoe.edu>
2018-11-12 11:30:21 -0600

    Updated add parts fields tests so that they are more robust

--- src/test/java/InventoryController/AddPartTextFieldValidationTest.java
+++ src/test/java/InventoryController/AddPartTextFieldValidationTest.java
@@ -76,7 +76,135 @@ public class AddPartTextFieldValidationTest {
         double correctValue = -1;
 
         //Act
+        double testValue = controllerAddPart.priceCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If zero into price textfield, -1 should be returned as a sign that the value is invalid.
+     */
+    @Test
+    public void enteringZeroIntoPriceFieldShouldReturnNegativeOne(){
+        //Arrange
+        String invalidInput = "0";
+        double correctValue = -1;
+
+        //Act
+        double testValue = controllerAddPart.priceCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If zero into quantity textfield, -1 should be returned as a sign that the value is invalid.
+     */
+    @Test
+    public void enteringZeroIntoQuantityFieldShouldReturnNegativeOne(){
+        //Arrange
+        String invalidInput = "0";
+        double correctValue = -1;
+
+        //Act
         double testValue = controllerAddPart.quantityCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If nominal value is entered into price textfield, the value should be returned as a sign that the value is valid.
+     */
+    @Test
+    public void enteringNominalValueIntoPriceFieldShouldReturnNominalValue(){
+        //Arrange
+        String invalidInput = "25";
+        double correctValue = 25;
+
+        //Act
+        double testValue = controllerAddPart.priceCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If nominal value is entered into quantity textfield, the value should be returned as a sign that the value is valid.
+     */
+    @Test
+    public void enteringNominalValueIntoQuantityFieldShouldReturnNominalValue(){
+        //Arrange
+        String invalidInput = "25";
+        double correctValue = 25;
+
+        //Act
+        double testValue = controllerAddPart.quantityCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If -1 is entered into price textfield, the value returned should be -1, indicating it's not valid
+     */
+    @Test
+    public void enteringNegativeOnceIntoPriceFieldShouldReturnNegativeOne(){
+        //Arrange
+        String invalidInput = "-1";
+        double correctValue = -1;
+
+        //Act
+        double testValue = controllerAddPart.priceCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If nominal value is entered into quantity textfield, the value returned should be -1, indicating it's not valid
+     */
+    @Test
+    public void enteringNegativeOneIntoQuantityFieldShouldReturnNegativeOne(){
+        //Arrange
+        String invalidInput = "-1";
+        double correctValue = -1;
+
+        //Act
+        double testValue = controllerAddPart.quantityCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If one is entered into quantity textfield, the value returned should be 1
+     */
+    @Test
+    public void enteringOneIntoQuantityFieldShouldReturnOne(){
+        //Arrange
+        String invalidInput = "1";
+        double correctValue = 1;
+
+        //Act
+        double testValue = controllerAddPart.quantityCheck(invalidInput);
+
+        //Assert
+        assertEquals(correctValue, testValue, 0.01);
+    }
+
+    /**
+     * If one is entered into price textfield, the value returned should be 1
+     */
+    @Test
+    public void enteringOneIntoPriceFieldShouldReturnOne(){
+        //Arrange
+        String invalidInput = "1";
+        double correctValue = 1;
+
+        //Act
+        double testValue = controllerAddPart.priceCheck(invalidInput);
 
         //Assert
         assertEquals(correctValue, testValue, 0.01);
