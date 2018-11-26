c6220039fb5ea3306a67ba516e7073dace3d09e9
Lang <langdk@ad.msoe.edu>
2018-11-26 13:18:57 -0600

    Changed test so that it tests all fields, and cleaned the format

--- src/test/java/InventoryControllerTestFX/AddPartTest.java
+++ src/test/java/InventoryControllerTestFX/AddPartTest.java
@@ -1,65 +1,63 @@
 package InventoryControllerTestFX;
-import InventoryController.ControllerAddPart;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
-
 import java.io.IOException;
 import java.net.URL;
-
-import static org.testfx.assertions.api.Assertions.assertThat;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.hasText;
 
 public class AddPartTest extends ApplicationTest {
-
-
 
     @Override
     public void start (Stage stage)throws IOException{
         URL myFxmlURL = ClassLoader.getSystemResource("AddPart.fxml");
         FXMLLoader loader = new FXMLLoader(myFxmlURL);
         Parent root = loader.load(myFxmlURL);
-        Scene scene = new Scene(root, 789, 620);
-        stage.setResizable(false);
-        stage.setTitle("Barcode Scanner");
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("AddPart Test");
         stage.setScene(scene);
         stage.show();
     }
 
-    @Test
-    public void testEnglishInput () {
-        clickOn("#nameField");
-        write("This is a test!");
 
+    @Test
+    public void verifyFieldInformation(){
+        //Arrange/Act
+        setupFields();
+
+        //Assert
+        verifyFields();
     }
 
+    private void setupFields(){
+        clickOn("#nameField");
+        write("Circuit Designers");
+        clickOn("#serialField");
+        write("100");
+        clickOn("#manufacturerField");
+        write("MSOE");
+        clickOn("#quantityField");
+        write("100");
+        clickOn("#priceField");
+        write("10");
+        clickOn("#vendorField");
+        write("MSOE");
+        clickOn("#locationField");
+        write("S351");
+    }
 
+    private void verifyFields(){
+        verifyThat("#nameField", hasText("Circuit Designers"));
+        verifyThat("#serialField", hasText("100"));
+        verifyThat("#manufacturerField", hasText("MSOE"));
+        verifyThat("#quantityField", hasText("100"));
+        verifyThat("#priceField", hasText("10"));
+        verifyThat("#vendorField", hasText("MSOE"));
+        verifyThat("#locationField", hasText("S351"));
+    }
 
-//    private final ControllerAddPart controllerAddPart = new ControllerAddPart();
-//
-//
-////    protected Parent getRootNode() throws IOException {
-////        Parent parent = null;
-////        try {
-////            parent = FXMLLoader.load(getClass().getResource("AddPart.fxml"));
-////            return parent;
-////        }catch (IOException e){
-////            e.printStackTrace();
-////        }
-////        return parent;
-////    }
-//
-//    @Override
-//    public void start (Stage stage){
-//
-//    }
-//
-//    @Test
-//    public void hasAddButton(){
-//        TextField firstname = find("priceField");
-//        firstname.setText("23");
-//        verifyThat("#firstname", hasText("23"));
-//    }
 }
