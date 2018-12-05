50749ba9196a6808e025653e4c94c76a57fb32a9
terrybc <terrybc@msoe.edu>
2018-12-04 22:36:03 -0600

    started the overdue details

--- src/main/java/InventoryController/ControllerOverdueTab.java
+++ src/main/java/InventoryController/ControllerOverdueTab.java
@@ -4,19 +4,29 @@ import Database.Database;
 import Database.OverdueItems;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 public class ControllerOverdueTab extends ControllerInventoryPage implements Initializable {
+
+    @FXML
+    private AnchorPane overduePage;
 
     @FXML
     private TableView<OverdueItems> overdueItems;
@@ -39,10 +49,30 @@ public class ControllerOverdueTab extends ControllerInventoryPage implements Ini
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        overdueItems.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                Stage stage = new Stage();
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/OverduePopup.fxml"));
+                    Parent root = loader.load();
+//
+////                    ((ControllerOverdueTab) loader.getController()).init(overdueItems.getSelectionModel().getSelectedItem());
+                    Scene scene = new Scene(root, 400, 400);
+                    stage.setTitle("Overdue Item");
+//                    stage.initOwner(overduePage.getScene().getWindow());
+//                    stage.initModality(Modality.WINDOW_MODAL);
+                    stage.setScene(scene);
+                    stage.getIcons().add(new Image("msoe.png"));
+                    stage.show();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
         Label emptytableLabel = new Label("No parts found.");
         emptytableLabel.setFont(new Font(18));
         DecimalFormat df = new DecimalFormat("#,###,##0.00");
-        overdueItems.setPlaceholder(emptytableLabel);
+//        overdueItems.setPlaceholder(emptytableLabel);
         database = new Database();
         data = database.getOverdue();
         for (int i = 0; i < data.size(); i++){
@@ -50,6 +80,26 @@ public class ControllerOverdueTab extends ControllerInventoryPage implements Ini
             data.get(i).setPrice("$" + df.format(p));
         }
         populteTable();
+//        overdueItems.setOnMouseClicked(event -> {
+//            if (event.getClickCount() == 2) {
+//                Stage stage = new Stage();
+//                try {
+//                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/OverduePopup.fxml"));
+//                    Parent root = loader.load();
+////
+//////                    ((ControllerOverdueTab) loader.getController()).init(overdueItems.getSelectionModel().getSelectedItem());
+//                    Scene scene = new Scene(root, 400, 400);
+//                    stage.setTitle("Overdue Item");
+////                    stage.initOwner(overduePage.getScene().getWindow());
+////                    stage.initModality(Modality.WINDOW_MODAL);
+//                    stage.setScene(scene);
+//                    stage.getIcons().add(new Image("msoe.png"));
+//                    stage.show();
+//                }catch (IOException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
     }
 
     /**
