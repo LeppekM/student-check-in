a72d1589bd6a877dba0718710a6552076de0f053
Gilpin <gilpinj@ad.msoe.edu>
2018-12-13 10:26:23 -0600

    Test adding edit button to row in total tab table row.

--- src/main/java/InventoryController/TotalTabTableRow.java
+++ src/main/java/InventoryController/TotalTabTableRow.java
@@ -3,16 +3,22 @@ package InventoryController;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class TotalTabTableRow extends RecursiveTreeObject<TotalTabTableRow> {
 
     private StringProperty partName, serialNumber, location, barcode, partID;
 
+    private HBox buttons;
+
     private boolean fault;
 
-    public TotalTabTableRow(String partName, String serialNumber, String location,
+    public TotalTabTableRow(String partName, HBox buttons, String serialNumber, String location,
                             String barcode, boolean fault, String partID) {
         this.partName = new SimpleStringProperty(partName);
+        this.buttons = buttons;
+
         this.serialNumber = new SimpleStringProperty(serialNumber);
         this.location = new SimpleStringProperty(location);
         this.barcode = new SimpleStringProperty(barcode);
@@ -22,6 +28,10 @@ public class TotalTabTableRow extends RecursiveTreeObject<TotalTabTableRow> {
 
     public StringProperty getPartName() {
         return partName;
+    }
+
+    public HBox getButtons() {
+        return buttons;
     }
 
     public StringProperty getSerialNumber() {
