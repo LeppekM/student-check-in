6216bd0d0cc9fba72bd1687ceba567b98fa6a994
terrybc <terrybc@msoe.edu>
2018-12-20 17:00:34 -0600

    saved item only once

--- src/main/java/CheckItemsController/CheckoutPopUp.java
+++ src/main/java/CheckItemsController/CheckoutPopUp.java
@@ -18,6 +18,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
+import java.util.stream.IntStream;
 
 public class CheckoutPopUp extends StudentPage {
 
@@ -44,17 +45,25 @@ public class CheckoutPopUp extends StudentPage {
     public void savePart(ActionEvent actionEvent) {
         database = new Database();
         Student s = StudentPage.getStudent();
-//        if (!s.getSavedItems().contains(new SavedPart(student.getText(), part.getText(), date.getText(),
-//                Integer.parseInt(quantity.getText()), new Date(System.currentTimeMillis()).toString(), dueDate.getText(), cID.getText()))) {
+        int[] sID = new int[s.getSavedItems().size()];
+        boolean result = false;
+        for (int i = 0; i < s.getSavedItems().size(); i++) {
+            sID[i] = Integer.parseInt(s.getSavedItems().get(i).getCheckID());
+        }
+        for (int j = 0; j < s.getCheckedOut().size(); j++) {
+            int coID = s.getCheckedOut().get(j).getCheckID().get();
+            result = IntStream.of(sID).anyMatch(x -> x == coID);
+        }
+        if (result){
             String prof = JOptionPane.showInputDialog(null, "Please enter a Professors name");
             String course = JOptionPane.showInputDialog(null, "Please enter a course code (i.e. CS3840)");
             String reason = JOptionPane.showInputDialog(null, "Please enter a short description why they need it saved");
             s.getSavedItems().add(new SavedPart(student.getText(), part.getText(), date.getText(), Integer.parseInt(quantity.getText()),
                     new Date(System.currentTimeMillis()).toString(), dueDate.getText(), cID.getText(), prof, course, reason));
-//        }else {
-//            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Part has already been saved");
-//            alert.showAndWait();
-//        }
+        }else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Part has already been saved");
+            alert.showAndWait();
+        }
         try {
             Connection connection = database.getConnection();
             Statement statement = connection.createStatement();
