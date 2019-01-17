82229e962990271a5f949d2884a7678777676c0d
terrybc <terrybc@msoe.edu>
2019-01-17 09:21:53 -0600

    changed some styling on the student page

--- src/main/java/CheckItemsController/StudentPage.java
+++ src/main/java/CheckItemsController/StudentPage.java
@@ -94,7 +94,11 @@ public class StudentPage {
         fees.setText("Outstanding fees: $" + overdueFees);
         fees.getStylesheets().add(getClass().getResource("/css/HeaderStyle.css").toExternalForm());
         date = new Label("");
-        date.setText("Date of last rental: " + student.getDate());
+        if(student.getDate() == null){
+            date.setText("Date of last rental: Never");
+        }else {
+            date.setText("Date of last rental: " + student.getDate());
+        }
         date.getStylesheets().add(getClass().getResource("/css/HeaderStyle.css").toExternalForm());
         vbox.getChildren().add(studentName);
         vbox.getChildren().add(email);
