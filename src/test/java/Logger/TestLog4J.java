0d877ce9ec55fbe233b65a40475b1fe61f2db80a
Matthew Karcz <karczma@msoe.edu>
2019-02-15 22:01:48 -0600

    Added proper test for logger using Log4J library

--- src/test/java/Logger/TestLog4J.java
+++ src/test/java/Logger/TestLog4J.java
@@ -3,7 +3,14 @@ package Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import Logging.Log4J;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestLog4J {
 
@@ -11,12 +18,22 @@ public class TestLog4J {
 
     static {
         Log4J.enableForClass(TestLog4J.class);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        System.setProperty("current.date", dateFormat.format(new Date()));
+    }
+
+    @Before
+    public void createLogFile() {
+        logger.debug("Hello this is a debug message");
+        logger.error("This is an error message");
+        logger.warn("This is a warning");
+        logger.info("Hello this is a info message");
     }
 
     @Test
-    public static void testDebug() {
-        logger.debug("Hello this is a debug message");
-        System.out.println("Print In Log File");
-        logger.info("Hello this is a info message");
+    public void checkLogFileExists(){
+        File file = new File(System.getProperty("user.dir") + "\\latest.log");
+        assertEquals(file.exists(), true);
+        assertEquals(file.isDirectory(), false);
     }
 }
