bea61fe0bb5faa0fa347b4e440c69156e827bfc9
Gilpin <gilpinj@ad.msoe.edu>
2019-04-09 22:29:03 -0500

    Removed unnecessary worker object from overduepopupcontroller.

--- src/main/java/InventoryController/OverduePopUpController.java
+++ src/main/java/InventoryController/OverduePopUpController.java
@@ -11,8 +11,6 @@ import java.text.DecimalFormat;
 
 public class OverduePopUpController {
 
-    private Worker worker;
-
     @FXML
     private JFXTextField nameField = new JFXTextField();
     @FXML
