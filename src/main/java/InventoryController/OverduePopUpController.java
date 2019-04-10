12f36f54d6d2586837040b0e0bc668485ab10a9a
Gilpin <gilpinj@ad.msoe.edu>
2019-04-09 22:28:09 -0500

    Fixed issue with overdue pop up throwing error when called in any of 3 windows.

--- src/main/java/InventoryController/OverduePopUpController.java
+++ src/main/java/InventoryController/OverduePopUpController.java
@@ -2,13 +2,16 @@ package InventoryController;
 
 import Database.*;
 import Database.ObjectClasses.Student;
+import Database.ObjectClasses.Worker;
 import Database.OverdueItem;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 
 import java.text.DecimalFormat;
 
-public class OverduePopUpController extends ControllerOverdueTab {
+public class OverduePopUpController {
+
+    private Worker worker;
 
     @FXML
     private JFXTextField nameField = new JFXTextField();
