a07ae8c645fa709b84c84b66cf894c2f9a2bc86d
Daniel Lang <langdk@msoe.edu>
2020-03-18 10:38:06 -0500

    test

--- src/main/java/Database/StudentInfo.java
+++ src/main/java/Database/StudentInfo.java
@@ -14,7 +14,7 @@ public class StudentInfo {
     private final String getStudentNameFromIDQuery = "\n" +
             "select studentName from students\n" +
             "where studentID = ?";
-    private final String createNewStudent ="insert into students(studentID, email, studentName, createdAt, createdBy)\n" +
+    private final String createNewStudent = "insert into students(studentID, email, studentName, createdAt, createdBy)\n" +
             "values(?,?,?,?,?);";
     private final String getStudentNameFromEmailQuery = "select studentName from students where email = ?";
 
@@ -22,17 +22,16 @@ public class StudentInfo {
     private StageWrapper stageWrapper = new StageWrapper();
 
 
-
-    public Student selectStudentClean(String email){
+    public Student selectStudentClean(String email) {
         String query = "Select studentName, email, studentID from students where email = ?";
         String studentEmail = "";
-        String name ="";
+        String name = "";
         int id = 0;
-        try( Connection connection = DriverManager.getConnection(url, Database.username, Database.password)){
+        try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, email);
             ResultSet rs = statement.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 studentEmail = rs.getString("email");
                 name = rs.getString("studentName");
                 id = rs.getInt("studentID");
@@ -41,17 +40,17 @@ public class StudentInfo {
             StudentCheckIn.logger.error("SQLException: Can't connect to the database.");
             throw new IllegalStateException("Cannot connect the database", e);
         }
-        return new Student(name,id,studentEmail);
+        return new Student(name, id, studentEmail);
     }
 
-    public String getStudentNameFromID(String studentID){
+    public String getStudentNameFromID(String studentID) {
         String sName = "";
         try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(getStudentNameFromIDQuery);
             statement.setString(1, studentID);
             ResultSet rs = statement.executeQuery();
-            if(rs.next()){
-                sName= rs.getString("studentName");
+            if (rs.next()) {
+                sName = rs.getString("studentName");
             }
         } catch (SQLException e) {
             StudentCheckIn.logger.error("IllegalStateException: Can't connect to the database when looking for student.");
@@ -60,14 +59,14 @@ public class StudentInfo {
         return sName;
     }
 
-    public boolean studentHasItemsCheckedOut(int studentID){
+    public boolean studentHasItemsCheckedOut(int studentID) {
         String query = "select studentID from checkout where studentID =? and checkinAt is null";
         List<Integer> checkouts = new ArrayList<>();
         try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, studentID);
             ResultSet rs = statement.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 checkouts.add(rs.getInt("studentID"));
             }
         } catch (SQLException e) {
@@ -76,15 +75,16 @@ public class StudentInfo {
         }
         return !checkouts.isEmpty();
     }
-    public String getStudentIDFromEmailActual(String email){
+
+    public String getStudentIDFromEmailActual(String email) {
         String sName = "";
         String query = "select studentID from students where email = ?";
         try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, email);
             ResultSet rs = statement.executeQuery();
-            if(rs.next()){
-                sName= rs.getString("studentID");
+            if (rs.next()) {
+                sName = rs.getString("studentID");
             }
         } catch (SQLException e) {
             StudentCheckIn.logger.error("IllegalStateException: Can't connect to the database when looking for student.");
@@ -94,36 +94,42 @@ public class StudentInfo {
 
     }
 
-    public boolean getStudentIDFromEmail(String email){
+    public boolean getStudentIDFromEmail(String email) {
         String sName = "";
         String query = "select studentID from students where email = ?";
         try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, email);
             ResultSet rs = statement.executeQuery();
-            if(rs.next()){
-                sName= rs.getString("studentID");
+            if (rs.next()) {
+                sName = rs.getString("studentID");
             }
         } catch (SQLException e) {
             StudentCheckIn.logger.error("IllegalStateException: Can't connect to the database when looking for student.");
             throw new IllegalStateException("Cannot connect to the database", e);
         }
-        if (sName==null){
+        if (sName == null) {
             return true;
         }
         return false;
 
     }
-    public boolean studentHasCheckedOutItems2(String email){
 
-        int studentID = Integer.parseInt(getStudentIDFromEmailActual(email));
+    public boolean studentHasCheckedOutItems2(String email) {
+        int studentID = 0;
+
+        try {
+            studentID = Integer.parseInt(getStudentIDFromEmailActual(email));
+        } catch (NumberFormatException e) {
+            return false;
+        }
         return studentHasItemsCheckedOut(studentID);
     }
 
-    public void updateStudent(String studentEmail, int studentID){
+    public void updateStudent(String studentEmail, int studentID) {
 
 
-        if(studentHasCheckedOutItems2(studentEmail)){
+        if (studentHasCheckedOutItems2(studentEmail)) {
             stageWrapper.errorAlert("User has parts already checked out, please resolve this issue before proceeding. IF YOU COMPLETE THIS TRANSACTION IT WILL NOT ACTUALLY GO THROUGH! TALK TO JIM");
             return;
         }
@@ -133,7 +139,7 @@ public class StudentInfo {
 
         try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1,studentID);
+            preparedStatement.setInt(1, studentID);
             preparedStatement.setString(2, studentEmail);
             preparedStatement.execute();
             preparedStatement.close();
@@ -143,21 +149,21 @@ public class StudentInfo {
         }
     }
 
-    private PreparedStatement createNewStudentHelper(int studentID, String email, String studentName, PreparedStatement preparedStatement){
+    private PreparedStatement createNewStudentHelper(int studentID, String email, String studentName, PreparedStatement preparedStatement) {
         try {
             preparedStatement.setInt(1, studentID);
             preparedStatement.setString(2, email);
             preparedStatement.setString(3, studentName);
             preparedStatement.setString(4, helper.getCurrentDate());
             preparedStatement.setString(5, "Jim");//Hardcoded for now
-        }catch (SQLException e){
+        } catch (SQLException e) {
             StudentCheckIn.logger.error("IllegalStateException: Can't connect to the database when looking for student.");
             throw new IllegalStateException("Cannot connect to the database", e);
         }
         return preparedStatement;
     }
 
-    public void createNewStudent(int studentID, String email, String studentName){
+    public void createNewStudent(int studentID, String email, String studentName) {
         try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(createNewStudent);
             createNewStudentHelper(studentID, email, studentName, statement).execute();
@@ -168,24 +174,24 @@ public class StudentInfo {
         }
     }
 
-    public void createNewStudent(Student s){
-        try(Connection connection = DriverManager.getConnection(url, Database.username, Database.password)){
+    public void createNewStudent(Student s) {
+        try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(createNewStudent);
             createNewStudentHelper(s.getRFID(), s.getEmail(), s.getName(), statement);
             statement.close();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             StudentCheckIn.logger.error("IllegalStateException: Can't connect to the database when looking for student.");
             throw new IllegalStateException("Cannot connect to the database", e);
         }
     }
 
-    public String getStudentNameFromEmail(String email){
+    public String getStudentNameFromEmail(String email) {
         String sName = "";
         try (Connection connection = DriverManager.getConnection(url, Database.username, Database.password)) {
             PreparedStatement statement = connection.prepareStatement(getStudentNameFromEmailQuery);
             statement.setString(1, email);
             ResultSet rs = statement.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 sName = rs.getString("studentName");
             }
         } catch (SQLException e) {
